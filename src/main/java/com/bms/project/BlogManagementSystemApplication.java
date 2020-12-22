584fe3fc38d35455b9e02b06db8cfef2c25f7f2c
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-12-22 15:26:51 +0200

    initialize project
    first commit to start the project

---
+++ src/main/java/com/bms/project/BlogManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bms.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManagementSystemApplication.class, args);
+	}
+
+}
